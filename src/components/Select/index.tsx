1ed5f4fe1933bbf174658a049ff970f6c94ff016
Jongkeun <dldkfod@naver.com>
2019-10-18 14:03:40 +0900

    removed Hour component and divided to select style.css

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import "./style.css";
 
 interface SelectProps {
   children?: ReactNode;
--- src/components/SettingPage/index.tsx
+++ src/components/SettingPage/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Hours from "../Hours";
 import storage from "../../IO/storage";
 import Select from "../Select";
 
