10c34b0384ac16d76e2257b73a3cd4bd2af9f6fb
Jongkeun <dldkfod@naver.com>
2019-11-01 15:18:57 +0900

    version 1.0.2

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -42,20 +42,11 @@ const Timer = (props: any) => {
 
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-    );
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result in the element with id="demo"
     setTimeString(hours + "h " + minutes + "m " + seconds + "s ");
-
-    //   // If the count down is finished, write some text
-    //   if (distance < 0) {
-    //     clearInterval(x);
-    //     document.getElementById("demo").innerHTML = "EXPIRED";
-    //   }
   };
 
   const onClick = () => {
